8956431865b500c1369912816e1d3fe65d8c1921
redacraft46 <reda.regam@gmail.com>
2025-05-01 18:57:06 +0200

    Ajout du modèle .env.example pour la config

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+// config.php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$dbHost = $_ENV['DB_HOST'];
+$dbName = $_ENV['DB_NAME'];
+$dbUser = $_ENV['DB_USER'];
+$dbPass = $_ENV['DB_PASS'];
+
+$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=utf8mb4";
+try {
+    $pdo = new PDO($dsn, $dbUser, $dbPass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    die("Erreur DB : " . $e->getMessage());
+}
