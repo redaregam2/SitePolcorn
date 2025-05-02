014d480d62fca4f610d55f374491432fb4c1127d
redacraft46 <reda.regam@gmail.com>
2025-05-02 23:18:22 +0200

    Feature: add Font Awesome stylesheet for enhanced icon support in leaderboard pages

--- public_html/DevineInfini/leaderboard-infini.php
+++ public_html/DevineInfini/leaderboard-infini.php
@@ -5,6 +5,7 @@
   <title>Leaderboard – Mode Infini</title>
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <link rel="stylesheet" href="css/leaderboard.css">
   <link rel="stylesheet" href="/css/background.css">
 </head>
