134e190c828d7a11ec60e13e0568992c9aa45edb
redacraft46 <reda.regam@gmail.com>
2025-05-02 22:53:47 +0200

    Refactor: add viewport meta tag for responsive design in soumettre.php and soumettre-emoji.php; update h1 font size in admin-style.css and emoji-admin.css -> En gros formulaire responsive

--- devine-le-film/soumettre.php
+++ devine-le-film/soumettre.php
@@ -85,6 +85,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
   <meta charset="UTF-8">
   <title>Soumettre une affiche – POLCORN</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/admin-style.css">
   <link rel="stylesheet" href="/css/background.css">
